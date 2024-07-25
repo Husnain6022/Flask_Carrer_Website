d354892169a4bfcfa5444548df2a5aef5e414628
Husnain Iftikhar <146443727+Husnain6022@users.noreply.github.com>
2024-07-25 08:32:36 +0000

    requirements added

--- app.py
+++ app.py
@@ -1,12 +1,43 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, jsonify
 
 app = Flask(__name__)
+
+JOBS = [
+  {
+    'id': 1,
+    'title': 'Data Analyst',
+    'location': 'Lahore',
+    'salary': 'Rs. 120k'
+  },
+  {
+    'id': 2,
+    'title': 'Data Engenier',
+    'location': 'Karachi',
+    'salary': 'Rs. 220k'
+  },
+  {
+    'id': 3,
+    'title': 'Frontend Developer',
+    'location': 'Islamabad',
+  
+  },
+  {
+    'id': 4,
+    'title': 'Backend Engenier',
+    'location': 'Lahore',
+    'salary': 'Rs. 420k'
+  }
+]
 
 
 @app.route('/')
 def hello_world():
-  return render_template('home.html')
+  return render_template('home.html', jobs=JOBS)
 
+
+@app.route('/api/jobs')
+def lis_jobs():
+  return jsonify(JOBS)
 
 if __name__ == '__main__':
   app.run(host='0.0.0.0', debug=True)
